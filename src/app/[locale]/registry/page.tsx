fca1ec3299a10724d7fa0f45ccde8dcfe29bc0b7
Jen Hail <hi@jenhail.com>
2024-04-17 15:47:18 -0400

    add registry

--- src/app/[locale]/registry/page.tsx
+++ src/app/[locale]/registry/page.tsx
@@ -2,6 +2,7 @@ import s from './page.module.scss'
 import { useTranslations } from 'next-intl'
 import { unstable_setRequestLocale } from 'next-intl/server'
 import { Link } from '@/navigation'
+import Script from 'next/script'
 
 export default function Home({
   params: { locale },
@@ -20,6 +21,14 @@ export default function Home({
           <i>Registry</i>
         </h1>
         <p>We need to write this page</p>
+        <a
+          className="zola-registry-embed"
+          href="www.zola.com/registry/hailvillavicenciowedding"
+          data-registry-key="hailvillavicenciowedding"
+        >
+          Our Zola Wedding Registry
+        </a>
+        <Script src="/registry.js" />
       </section>
     </>
   )
