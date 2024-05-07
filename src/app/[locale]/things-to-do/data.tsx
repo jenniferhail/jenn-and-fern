b72743906c88855ec6dc9be0a1db8a85020be6c4
Jen Hail <hi@jenhail.com>
2024-05-07 00:12:27 -0400

    adding links to things to do

---
+++ src/app/[locale]/things-to-do/data.tsx
@@ -0,0 +1,176 @@
+export const thingsToDo = [
+  {
+    name: 'coffee',
+    items: [
+      {
+        title: 'Dozer Coffee',
+        link: 'https://maps.app.goo.gl/NKaV6NSNoMxaZR8S7',
+      },
+      {
+        title: 'Comet Coffee',
+        link: 'https://maps.app.goo.gl/j6e8Z8xp7qGa7acL9',
+      },
+      {
+        title: 'Argus Farm Stop',
+        link: 'https://maps.app.goo.gl/d5CCEzyGKiSUfNdN8',
+      },
+      {
+        title: 'Lowertown Bar & Cafe',
+        link: 'https://maps.app.goo.gl/hKNg46XeHAJFzzis9',
+      },
+      {
+        title: 'The Drip House',
+        link: 'https://maps.app.goo.gl/ARScx9xZhxb5hBFAA',
+      },
+    ],
+  },
+  {
+    name: 'brunch',
+    items: [
+      {
+        title: 'Stray Hen',
+        link: 'https://maps.app.goo.gl/M2B7kAobs67z1vDR7',
+      },
+      {
+        title: "Zingerman's Delicatessen",
+        link: 'https://maps.app.goo.gl/XDfkvvEY2v1wXSfv6',
+      },
+      {
+        title: 'Kerrytown Market',
+        link: 'https://maps.app.goo.gl/sFJ5QyEUmSDavtEV9',
+      },
+    ],
+  },
+  {
+    name: 'food',
+    items: [
+      {
+        title: 'Tomukun Noodle Bar',
+        link: 'https://maps.app.goo.gl/16DaVGnq6UnKDTHR8',
+      },
+      {
+        title: 'York Food & Drink',
+        link: 'https://maps.app.goo.gl/T6v9qiGEuY8CZuMS7',
+      },
+      {
+        title: 'Zamaan Cafe',
+        link: 'https://maps.app.goo.gl/agAc2MuGuoeGcCaLA',
+      },
+      {
+        title: 'Jerusalem Garden',
+        link: 'https://maps.app.goo.gl/CFii9DdbfBecEdPq5',
+      },
+      {
+        title: 'Mani Osteria & Bar',
+        link: 'https://maps.app.goo.gl/hqam4a9h3Dq8YDgu8',
+      },
+      {
+        title: 'Aventura',
+        link: 'https://maps.app.goo.gl/NoPCdCkDTG7NV4sDA',
+      },
+      {
+        title: "Zingerman's Roadhouse",
+        link: 'https://maps.app.goo.gl/tGonh4orzbf2UWrt9',
+      },
+      {
+        title: 'Fleetwood Diner',
+        link: 'https://maps.app.goo.gl/jRZcFDFKmEz4im2aA',
+      },
+      {
+        title: 'Blank Slate Creamery',
+        link: 'https://maps.app.goo.gl/W6BCE9vLUanHPsFb7',
+      },
+    ],
+  },
+  {
+    name: 'drinks',
+    items: [
+      {
+        title: 'Peridot',
+        link: 'https://maps.app.goo.gl/1BW7VTygndu8BhQf7',
+      },
+      {
+        title: 'Nightcap',
+        link: 'https://maps.app.goo.gl/LAyE3t4cACXqiCDd7',
+      },
+      {
+        title: 'The Last Word',
+        link: 'https://maps.app.goo.gl/adhXhPxGpraHrQ7KA',
+      },
+      {
+        title: 'Blom Mead + Cider (A great spot for board games!)',
+        link: 'https://maps.app.goo.gl/9baBZRBfGGBQVUWSA',
+      },
+      {
+        title: 'Lowertown Bar & Cafe',
+        link: 'https://maps.app.goo.gl/hKNg46XeHAJFzzis9',
+      },
+      {
+        title: "Bill's Beer Garden",
+        link: 'https://maps.app.goo.gl/cyT6qxQptEQruQPh8',
+      },
+    ],
+  },
+  {
+    name: 'in',
+    items: [
+      {
+        title: 'Nickels Arcade',
+        link: 'https://maps.app.goo.gl/kUgfLZXZCEzUBcR1A',
+      },
+      {
+        title: 'Canoeing & Kayaking at Argo Park ',
+        link: 'https://maps.app.goo.gl/vGQ1TSDHkhYUJnU89',
+      },
+      {
+        title: 'Nichols Arboretum',
+        link: 'https://maps.app.goo.gl/u81zddsj3ugxvuor7',
+      },
+      {
+        title: 'University of Michigan Museum of Art',
+        link: 'https://maps.app.goo.gl/88YvBVcLjuUptDES7',
+      },
+      {
+        title: 'University of Michigan Museum of Natural History',
+        link: 'https://maps.app.goo.gl/V4dxmdVRCfKihuk37',
+      },
+      {
+        title: 'Kerrytown Market',
+        link: 'https://maps.app.goo.gl/sFJ5QyEUmSDavtEV9',
+      },
+      {
+        title: 'The State Theater',
+        link: 'https://maps.app.goo.gl/mYXY7D8tAMWi2rjp6',
+      },
+    ],
+  },
+  {
+    name: 'out',
+    items: [
+      {
+        title: 'Detroit Institute of Arts',
+        link: 'https://maps.app.goo.gl/FPmF4gnBAcnzDEeR6',
+      },
+      {
+        title: 'Takoi - Our favorite Thai Restaurant',
+        link: 'https://maps.app.goo.gl/fVVpCp5fKPMv54Mg8',
+      },
+      {
+        title: 'Belle Isle Park',
+        link: 'https://maps.app.goo.gl/uDRfX8JJvBNFFMvU7',
+      },
+      {
+        title: 'LEGOLAND Discovery Center Michigan',
+        link: 'https://maps.app.goo.gl/m4iAfBqyDLWwiZoQ8',
+      },
+      {
+        title: 'Great Lakes Crossing Outlets',
+        link: 'https://maps.app.goo.gl/cQn6jmB3PWyMYKCP6',
+      },
+      {
+        title: 'Canada',
+        link: '',
+      },
+    ],
+  },
+]
--- src/app/[locale]/things-to-do/page.tsx
+++ src/app/[locale]/things-to-do/page.tsx
@@ -2,6 +2,7 @@ import s from './page.module.scss'
 import { useTranslations } from 'next-intl'
 import { unstable_setRequestLocale } from 'next-intl/server'
 import { Link } from '@/navigation'
+import { thingsToDo } from './data'
 
 export default function Home({
   params: { locale },
@@ -22,60 +23,24 @@ export default function Home({
         <div dangerouslySetInnerHTML={{ __html: t.raw('intro') }} />
       </section>
       <section className={s.section}>
-        <h2>{t('coffee')}</h2>
-        <ul>
-          <li>Dozer Coffee</li>
-          <li>Comet Coffee</li>
-          <li>Argus Farm Stop</li>
-          <li>Lowertown Bar & Cafe</li>
-          <li>The Drip House</li>
-        </ul>
-        <h2>{t('brunch')}</h2>
-        <ul>
-          <li>Stray Hen</li>
-          <li>Zingerman&apos;s Deli</li>
-          <li>Kerrytown Market</li>
-        </ul>
-        <h2>{t('food')}</h2>
-        <ul>
-          <li>Tomukun</li>
-          <li>York Food & Drink</li>
-          <li>Zamaan Cafe</li>
-          <li>Jerusalem Garden</li>
-          <li>Mani Osteria & Bar</li>
-          <li>Aventura</li>
-          <li>Zingerman&apos;s Roadhouse</li>
-          <li>Fleetwood Diner</li>
-          <li>Blank Slate Creamery</li>
-        </ul>
-        <h2>{t('drinks')}</h2>
-        <ul>
-          <li>Nightcap</li>
-          <li>The Last Word</li>
-          <li>Blom Mead + Cider</li>
-          <li>Lowertown Bar & Cafe</li>
-          <li>Bill&apos;s Beer Garden</li>
-        </ul>
-        <h2>{t('in')}</h2>
-        <ul>
-          <li>Downtown</li>
-          <li>Tubing on the lake</li>
-          <li>Arboretum</li>
-          <li>Art museum</li>
-          <li>Natural history museum</li>
-          <li>Hands on museum</li>
-          <li>Kerry Town</li>
-          <li>Local Theater</li>
-        </ul>
-        <h2>{t('out')}</h2>
-        <ul>
-          <li>Detroit</li>
-          <li>Belle Isle</li>
-          <li>LegoLand</li>
-          <li>Canada</li>
-          <li>Outlet Mall</li>
-          <li>Takoi</li>
-        </ul>
+        {thingsToDo?.map((category) => (
+          <div key={category.name}>
+            <h2>{t(category.name)}</h2>
+            <ul>
+              {category.items.map((item, i) => (
+                <li key={i}>
+                  {item.link ? (
+                    <Link href={item.link} target="_blank">
+                      {item.title}
+                    </Link>
+                  ) : (
+                    item.title
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </section>
     </>
   )
